d3a0e8c92561e931e9db34ad2d9a5dcaf2a76f37
alvinirving <alvinirving1@yahoo.com>
2025-03-26 23:35:44 +0000

    Update styles - handling edge cases

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -2,7 +2,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-const config: Config = {
+let config: Config = {
   title: 'Formik MUI',
   tagline: 'Easily combine Formik with MUI',
   url: 'https://stackworx.github.io/',
